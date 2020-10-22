9ce577ccf509b1b997a7c1567180aa4151c740df
zhangwei <pknfe@outlook.com>
2020-10-22 22:41:55 +0800

    add test code

--- README.md
+++ README.md
@@ -3,18 +3,26 @@ Apache SkyWalking Client Test
 # Usage
 * Install  
 the skywalking-client-js runtime library is available at npm
-```
+
+```bash
 npm install skywalking-client-js --save
 ```
+
 * Init SDK
-```
+
+```typescript
 import ClientMonitor from 'skywalking-client-js';
-```
-```
+
 ClientMonitor.register({
   collector: 'http://localhost:8888',
   service: 'test-ui',
   pagePath: 'http://localhost:8080/',
   serviceVersion: 'v1.0.0',
 });
+```
+
+# Quick start
+
+```bash
+cd test && export OAP_SERVER=oap:12800 && docker-compose up -d
 ```
---
+++ test/test.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+from selenium import webdriver as wd
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities as DC
+
+hub_remote_url = os.environ.get("HUB_REMOTE_URL", "http://selenium-hub:4444/wd/hub")
+test_url = os.environ.get("TEST_URL", "http://test-ui:80/")
+
+
+def test_screenshot():
+    try:
+        driver.get(test_url)
+    except Exception as e:
+        print(e)
+
+
+try:
+    driver = wd.Remote(
+        command_executor=hub_remote_url,
+        desired_capabilities=DC.CHROME)
+
+    while True:
+        test_screenshot()
+        time.sleep(7)
+
+finally:
+    driver.quit()
